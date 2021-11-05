635e9475fbfcc525899acdcb7fa672d2a682ae49
carolainseija <carolainsilva1@gmail.com>
2021-11-05 13:39:29 -0300

    structure pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom"
 
 import './App.css';
+import Contact from './pages/Contact';
 import Index from './pages/Index';
 import Service from './pages/Service';
+import Work from './pages/Work';
 
 
 const App = () => {
   return (
   <Router>
     <Routes>
+      <Route path="/contacto" element={<Contact />} />
+      <Route path="/postulaciones" element={<Work />} />
       <Route path="/servicio" element={<Service />} />
       <Route path="/" element={<Index />} />
     </Routes>
