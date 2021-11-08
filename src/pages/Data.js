b18dbbad17947c56c7416b80a1870c8610941979
carolainseija <carolainsilva1@gmail.com>
2021-11-08 14:20:49 -0300

    form payment

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import FormPayment from '../components/Forms/Payment/Index';
+import Navbar from '../components/Navbar';
 
 
 const FormData = () => {
-    const select = "Auto"
+    const select = "Autos"
     const price = "250"
     return (
-        <div>
-            <p> Completa tus datos</p>
-            <p>has seleccionado {select}</p>
-            <p>{price}$</p>
+        <>
+            <Navbar />
+        <div className="data">
+            <p className="data_title">Completa tus datos</p>
+            <p>Has seleccionado la categoria:  {select}</p>
+            Total a pagar: 
+            <p className="text-price">{price}$</p>
             <FormPayment />
         </div>
+</>
     )
 }
 
