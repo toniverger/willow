97daae9670e6dfe0e536a3c36e85b711c036f012
carolainseija <carolainsilva1@gmail.com>
2021-11-08 11:13:53 -0300

    service style

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -8,19 +8,21 @@ const Service = () => {
         <>
             <Navbar />
             <div className="service in-row">
-                <div>
+                <div className="service_left">
                     <img src={carBlue} alt="auto azul" />
                 </div>
-                <div>
+                <div className="service_right">
                     <h className="subtitle">Lavado</h>
                     <p className="text-gray"> $200.00 - $250.00</p>
-                    <p>Lavado exterior realizado con hidrolavadoras a presión de bajo consumo en agua, utilización de jabones biodegradables que no contaminan, ni rayan la pintura del vehiculo. Lavado totalmente amigable con el medio ambiente. </p>
-                    <p className="text-gray">Tipo de vehículo</p>
-                    <select name="cars" id="cars">
-                        <option value="auto">Auto</option>
-                        <option value="camioneta">Camioneta o SVU</option>
-                    </select>
-                    <p>Limpiar</p>
+                    <p className="service_paragraph">Lavado exterior realizado con hidrolavadoras a presión de bajo consumo en agua, utilización de jabones biodegradables que no contaminan, ni rayan la pintura del vehiculo. Lavado totalmente amigable con el medio ambiente. </p>
+                    <div className="in-row type-transport">
+                        <p className="type-transport_type">Tipo de vehículo</p>
+                        <select className="select" name="cars" id="cars">
+                            <option value="auto">Auto</option>
+                            <option value="camioneta">Camioneta o SVU</option>
+                        </select>
+                        <p className="text-clean">Limpiar</p>
+                    </div>
                     <button className="btn-blue"><a className="text-white" href="lavado">Completar Datos</a></button>
                 </div>
             </div>
