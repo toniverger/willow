92421775cafd25175fa273f4ae12a4be1e0dc5fd
carolainseija <carolainsilva1@gmail.com>
2021-11-09 10:10:56 -0300

    page contact and component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import FormContact from '../components/Forms/Contact/Index';
+import Navbar from '../components/Navbar';
+import '../styles/styles.css'
 
 const Contact = () => {
-    return(
-        <h1>Contacto</h1>
+    return (
+        <>
+            <Navbar />
+            <div className="contact">
+                <div className="contact-titles">
+                    <h className="contact-subtitle">Contacto</h>
+                    <p className="contact-text">Si precisas contactarte podes hacerlo en el siguiente formulario:</p>
+                </div>
+                <div className="contact-form">
+                    <FormContact />
+                </div>
+            </div>
+        </>
     )
 }
 
