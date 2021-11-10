dbfb7ee322866e5c3a851b2eac4a8292c0afcdf4
carolainseija <carolainsilva1@gmail.com>
2021-11-10 09:46:52 -0300

    foms responsive

--- src/components/Forms/Payment/Index.js
+++ src/components/Forms/Payment/Index.js
@@ -22,11 +22,11 @@ const FormPayment = () => {
     return (
         <div>
             <form>
-                <input className="input" placeholder="Nombre Completo" type="text"></input>
-                <input className="input" placeholder="Marca" type="text"></input>
-                <input className="input" placeholder="Color" type="text"></input>
-                <input className="input" placeholder="Matricula" type="text"></input>
-                <textarea placeholder="Ubicación: Lo más detallada posible."></textarea>
+                <input className="input-service" placeholder="Nombre Completo" type="text"></input>
+                <input className="input-service" placeholder="Marca" type="text"></input>
+                <input className="input-service" placeholder="Color" type="text"></input>
+                <input className="input-service" placeholder="Matricula" type="text"></input>
+                <textarea className="textarea-service" placeholder="Ubicación: Lo más detallada posible."></textarea>
                 <div>
                     <p>Forma de pago </p>
                     <label>Al contado </label>
