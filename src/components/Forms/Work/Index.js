979d184a686899770d2a85034ab08fd77dfc0f3d
carolainseija <carolainsilva1@gmail.com>
2021-11-11 14:10:33 -0300

    inputs check

--- src/components/Forms/Work/Index.js
+++ src/components/Forms/Work/Index.js
@@ -17,16 +17,18 @@ const FormWork = () => {
                     <label>Estado civil: </label>
                     <input className="input-work" type="text"></input>
                 </div>
-                <div>
-                    <label>Tienes hijos: </label>
+                <div className="questions-work_option">
+                    <label className="label-option">Tienes hijos: </label>
+                <div className="questions-checkbox">
+                    <input className="inputRadio" type="radio"></input>
                     <label>Si</label>
+                    </div>
                     <input className="inputRadio" type="radio"></input>
                     <label>No</label>
-                    <input className="inputRadio" type="radio"></input>
                 </div>
                 <div className="questions-work">
-                    <label>Correo electrónico: </label>
                     <input className="input-work" type="text"></input>
+                    <label>Correo electrónico: </label>
                 </div>
                 <div className="questions-work">
                     <label>Departamento: </label>
@@ -38,23 +40,36 @@ const FormWork = () => {
                 </div>
                 <div>
                     <label>¿Tienes celular? </label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="radio"></input>
                     <label>Si</label>
+                    </div>
+                    <div className="questions-checkbox">
                     <input className="inputRadio" type="radio"></input>
                     <label>No</label>
+                    </div>
+                </div>
+                <div className>
+                    <label>¿Acceso a internet desde tu celular? </label>
+                    <div className="questions-checkbox">
                     <input className="inputRadio" type="radio"></input>
+                    <label>Si</label>
+               </div>
+               <div className="questions-checkbox">
+                    <input className="inputRadio" type="radio"></input>
+                    <label>No</label>
+                    </div>
                 </div>
                 <div>
-                    <label>¿Acceso a internet desde tu celular? </label>
-                    <label>Si</label>
-                    <input className="inputRadio" type="radio"></input>
-                    <label>No</label>
-                    <input className="inputRadio" type="radio"></input>
-                </div> <div>
                     <label>¿Tiene mono tributo?</label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="radio"></input>
                     <label>Si</label>
+                    </div>
+                    <div className="questions-checkbox">
                     <input className="inputRadio" type="radio"></input>
                     <label>No</label>
-                    <input className="inputRadio" type="radio"></input>
+                    </div>
                 </div> 
                 <div className="questions-work">
                     <label>Disponibilidad horaria</label>
@@ -62,31 +77,43 @@ const FormWork = () => {
                 </div>
                 <div>
                     <label>¿Querría trabajar fines de semana?</label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="checkbox"></input>
                     <label>Si</label>
+                    </div>
                     <input className="inputRadio" type="checkbox"></input>
                     <label>No</label>
-                    <input className="inputRadio" type="checkbox"></input>
                 </div>
                 <div>
                     <label>¿Prefiere 4 o 6 horas?</label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="checkbox"></input>
                     <label>4 Horas</label>
+                    </div>
+                    <div className="questions-checkbox">
                     <input className="inputRadio" type="checkbox"></input>
                     <label>6 Horas</label>
-                    <input className="inputRadio" type="checkbox"></input>
+                </div>
                 </div>
                 <div>
                     <label>¿Prefiere mañana o tarde?</label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="checkbox"></input>
                     <label>Mañana</label>
+                    </div>
                     <input className="inputRadio" type="checkbox"></input>
                     <label>Tarde</label>
-                    <input className="inputRadio" type="checkbox"></input>
                 </div>
                 <div>
                     <label>¿Tiene transporte propio?</label>
+                    <div className="questions-checkbox">
+                    <input className="inputRadio" type="checkbox"></input>
                     <label>Si</label>
+                    </div>
+                    <div className="questions-checkbox">
                     <input className="inputRadio" type="checkbox"></input>
                     <label>No</label>
-                    <input className="inputRadio" type="checkbox"></input>
+                </div>
                 </div>
                 <div className="questions-work">
                     <label>¿Libreta de moto?</label>
