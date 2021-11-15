34b5494dc0ee36ae33d981aa3d497845cb6a258e
carolainseija <carolainsilva1@gmail.com>
2021-11-15 15:00:20 -0300

    info for mail

--- src/components/Forms/Contact/Index.js
+++ src/components/Forms/Contact/Index.js
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
 
+
+
 const FormContact = () => {
+  const [infoMail, setInfoMail] = useState({});
+
+  const submitMensagge = () => {
+    console.log("verificar y enviar")
+  }
+
+
+  const information = (e) => {
+    setInfoMail({
+      ...infoMail,
+      [e.target.value]: e.target.value,
+    })
+  }
+
   return (
     <form className="form-contact">
       <div className="container-data">
         <div className="contact_in-col">
           <label className="requiered">NOMBRE <span>*</span></label>
-          <input required className="input" type="text"></input>
+          <input name="name" value={infoMail.name} onClick={(e) => information(e)} required className="input" type="text"></input>
         </div>
         <div className="contact_in-col">
           <label className="requiered">APELLIDO <span>*</span></label>
-          <input className="input" type="text"></input>
+          <input name="lastname" value={infoMail.lastname} className="input" type="text" onClick={(e) => information(e)} ></input>
         </div>
       </div>
       <div className="container-data2">
         <div className="contact_in-col emails">
           <label className="requiered">CORREO ELECTRÓNICO <span>*</span></label>
-          <input className="input_mail" type="text"></input>
+          <input name="mail" value={infoMail.mail} className="input_mail" type="text" onClick={(e) => information(e)} ></input>
         </div>
         <div className="contact_in-col">
           <label className="requiered">COMENTARIO O MENSAJE <span>*</span></label>
-          <textarea placeholder="Dejanos tu mensaje."></textarea>
+          <textarea name="mensagge" value={infoMail.mensagge} placeholder="Dejanos tu mensaje." onClick={(e) => information(e)} ></textarea>
         </div>
         <div className="contact_in-col">
-          <button className="btn-submit" type="submit">ENVIAR</button>
+          <button onClick={(e) => submitMensagge(e)} className="btn-submit" type="submit">ENVIAR</button>
         </div>
       </div>
     </form>
