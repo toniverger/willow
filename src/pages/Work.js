ae9cbe92d1b8048ccb203595ca77b486cc009275
carolainseija <carolainsilva1@gmail.com>
2021-11-16 12:48:30 -0300

    work subtitle

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -8,7 +8,7 @@ const Work = () => {
         <Navbar />
         <div className="work">
             <div className="work_texts">
-                <p className="subtitle">Quiero ser parte</p>
+                <p className="work_subtitle">Quiero ser parte</p>
                 <p className="wor_texts_p">Estas en el trabajo o en tu casa y tienes que lavar tu auto?, no pierdas tiempo y no te preocupes, con Willow podes hacer tu pedido online y se lava el auto sin necesidad de moverlo de su lugar.</p>
             </div>
             <div className="work_form">
