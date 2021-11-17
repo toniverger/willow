304ae8c911a84b7310012537255bc996cf2f0125
carolainseija <carolainsilva1@gmail.com>
2021-11-17 14:07:25 -0300

    texts

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -12,9 +12,7 @@ const Work = () => {
         <div className="work_texts">
           <p className="subtitle">QUIERO SER PARTE</p>
           <p className="wor_texts_p">
-            Estas en el trabajo o en tu casa y tienes que lavar tu auto?, no
-            pierdas tiempo y no te preocupes, con Willow podes hacer tu pedido
-            online y se lava el auto sin necesidad de moverlo de su lugar.
+          Queremos conocerte mejor, te invitamos a completar tus datos en el siguiente formulario.
           </p>
         </div>
         <div className="work_form">
