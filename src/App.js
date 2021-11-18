63c47f7bcf50cba663f1f97171adb0c9cda94b59
carolainseija <carolainsilva1@gmail.com>
2021-11-18 12:41:22 -0300

    forms and congrats

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Service from './pages/Service';
 import Work from './pages/Work';
 import FormData from './pages/Data';
 import Index from './pages/Index';
+import ThankYou from './pages/ThankYou';
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/postulaciones" element={<Work />} />
       <Route path="/lavado" element={<FormData />} />
       <Route path="/servicios" element={<Service />} />
+      <Route path="/gracias" element={<ThankYou />} />
       <Route path="/" element={<Index />} />
     </Routes>
   </Router>
--- src/components/Forms/Contact/Index.js
+++ src/components/Forms/Contact/Index.js
@@ -1,42 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import './style.css';
 
 const FormContact = () => {
-  const [infoMail, setInfoMail] = useState({});
-
-  const submitMensagge = (e) => {
-    e.preventDefault();
-    console.log("verificar y enviar")
-  }
-
-
-  const information = (e) => {
-    setInfoMail({
-      ...infoMail,
-      [e.target.name]: e.target.value,
-    })
-  }
 
   return (
-    <form className="form-contact" onSubmit={(e) => submitMensagge(e)}>
+    <form className="form-contact" action="https://formsubmit.co/03578cdb8b2903d06d30c6edb8aa3c78" method="POST">
+      <input type="hidden" name="_template" value="table"></input>
+      <input type="hidden" name="_subject" value="Willow, alguien se quiere poner en contacto!"></input>
+      <input type="hidden" name="_next" value="http://localhost:3000/gracias"></input>
+      <input type="hidden" name="_captcha" value="false"></input>
       <div className="container-data">
         <div className="contact_in-col">
           <label className="requiered">NOMBRE <span>*</span></label>
-          <input name="name" value={infoMail.name} onChange={(e) => information(e)} required className="input" type="text"></input>
+          <input className="input" type="text" name="Nombre" required></input>
         </div>
         <div className="contact_in-col">
           <label className="requiered">APELLIDO <span>*</span></label>
-          <input name="lastname" value={infoMail.lastname} className="input" type="text" onChange={(e) => information(e)}required></input>
+          <input className="input" type="text" name="Apellido" required></input>
         </div>
       </div>
       <div className="container-data2">
         <div className="contact_in-col emails">
           <label className="requiered">CORREO ELECTRÓNICO <span>*</span></label>
-          <input name="mail" value={infoMail.mail} className="input_mail" type="text" onChange={(e) => information(e)} required></input>
+          <input className="input_mail" type="email" name="Email"></input>
         </div>
         <div className="contact_in-col">
           <label className="requiered">COMENTARIO O MENSAJE <span>*</span></label>
-          <textarea name="mensagge" value={infoMail.mensagge} placeholder="Dejanos tu mensaje." onChange={(e) => information(e)} required></textarea>
+          <textarea name="Mensaje" placeholder="Dejanos tu mensaje." required></textarea>
         </div>
         <div className="contact_in-col">
           <button className="btn-submit" type="submit">ENVIAR</button>
